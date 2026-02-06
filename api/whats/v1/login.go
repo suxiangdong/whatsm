6142f6b0f08cc826f217d81db9dcf28f225adc45
suxiangdong <xiangdong.su@sunwayig.com>
2026-02-06 17:47:21 +0800

    修正命名

--- api/whats/v1/login.go
+++ api/whats/v1/login.go
@@ -4,7 +4,7 @@ import "github.com/gogf/gf/v2/frame/g"
 
 type LoginReq struct {
 	g.Meta `path:"/whats/login" tags:"whats" method:"post" sm:"登录"`
-	Phone  string `json:"jid" v:"required" dc:"手机号，带区号，不带+号"`
+	Phone  string `json:"phone" v:"required" dc:"手机号，带区号，不带+号"`
 	Proxy  string `json:"proxy" dc:"代理地址，示例：socks5://root:aasd1123@127.0.0.1:10808"`
 }
 
